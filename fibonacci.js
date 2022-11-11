22334b4ab6ee12a6256429ff1201bae2e8937b70
nitanshu <nitanshukhatri@yahoo.in>
2022-11-11 10:49:45 +0530

    adding more questions

--- fibonacci.js
+++ fibonacci.js
@@ -3,4 +3,18 @@ function fibonacci(n) {
   for (let i = 2; i < n; i++) {
     fib[i] = fib[i - 1] + fib[i - 2];
   }
+  return fib;
 }
+
+function fib(n) {
+  var a = 0,
+    b = 1;
+  for (var i = 0; i < n; i++) {
+    var temp = a + b;
+    a = b;
+    b = temp;
+  }
+  return a;
+}
+0;
+console.log(fibonacci(3));
