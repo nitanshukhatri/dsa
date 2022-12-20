b1b2c9e84b199dfac38c31b044afda4f9b599a55
nitanshu <nitanshukhatri@yahoo.in>
2022-12-20 15:20:37 +0530

    unique character

--- find-replace.js
+++ find-replace.js
@@ -9,15 +9,15 @@ var findReplaceString = function (S, indexes, sources, targets) {
   for (i = 0; i < S.length; i++) {
     let ist = map[i]; // ist = index source target length; coudnt come up with a better word
     if (ist) {
-      //console.log(ist[0]);
+      console.log(ist[0]);
       let len = ist[0].length;
       let sub = S.substring(i, i + len); // extract b/w charachter
       //console.log(sub);
       if (sub == ist[0]) {
         // if substring from original string equals source string
         str += ist[1]; // add target string instead of that
-        console.log(ist[1]);
-        console.log(i);
+        //console.log(ist[1]);
+        //console.log(i);
         i = i + len - 1; // skip extra letters in for loop iteration
         console.log(i);
       } else {
